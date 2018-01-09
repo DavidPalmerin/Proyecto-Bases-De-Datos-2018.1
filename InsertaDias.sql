0a0aca78812bae77a1afd94359f51f336aa0690c
BaruchAcevedo <goruch@ciencias.unam.mx>
2018-01-09 14:22:52 -0600

    Poblado de la Base

---
+++ InsertaDias.sql
@@ -0,0 +1,7 @@
+INSERT INTO Dias (id_dia,nombre_dia) VALUES (1,'Lunes');
+INSERT INTO Dias (id_dia,nombre_dia) VALUES (2,'Martes');
+INSERT INTO Dias (id_dia,nombre_dia) VALUES (3,'Miercoles');
+INSERT INTO Dias (id_dia,nombre_dia) VALUES (4,'Jueves');
+INSERT INTO Dias (id_dia,nombre_dia) VALUES (5,'Viernes');
+INSERT INTO Dias (id_dia,nombre_dia) VALUES (6,'Sabado');
+INSERT INTO Dias (id_dia,nombre_dia) VALUES (7,'Domingo');
